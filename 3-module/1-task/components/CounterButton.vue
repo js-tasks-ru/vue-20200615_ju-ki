57851e8449ce7ffb0b05e6aa07cc2c86d7009ffd
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-12-21 11:45:04 +0100

    3-1 CounterButton: task was done

---
+++ 3-module/1-task/components/CounterButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="onclick" type="button">{{count}}</button>
+</template>
+
+<script>
+
+export default {
+  name: 'CounterButton',
+
+  props: {
+    count: {
+      default: 0,
+      type: Number
+    }
+  },
+
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
+
+  methods: {
+    onclick() {
+      this.$emit('increment', this.count + 1);
+    }
+  }
+
+}
+</script>
