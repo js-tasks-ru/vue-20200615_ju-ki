0ff38a1d489ad2c66d0fe645b4ccad2cb5a84fd7
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2021-01-04 14:50:47 +0100

    4-4: LoginPage + RegisterPage was done

--- 4-module/4-task/views/LoginPage.vue
+++ 4-module/4-task/views/LoginPage.vue
@@ -1,12 +1,63 @@
 <template>
-  <form class="form"></form>
+  <form class="form" @submit.prevent="doLogin">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input type="email" @input="setEmail($event)" placeholder="demo@email" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input type="password" @input="setPassword($event)" placeholder="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary button_block">Войти</button>
+    </div>
+    <div class="form__append">Нет аккаунта? <router-link to="/register" class="link">Зарегистрируйтесь</router-link></div>
+  </form>
 </template>
 
 <script>
-// import { login } from '../data';
+import { login } from '../data';
 
 export default {
   name: 'LoginPage',
+
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  },
+
+  methods: {
+    setEmail(event) {
+      this.email = event.target.value
+    },
+
+    setPassword(event) {
+      this.password = event.target.value
+    },
+
+    doLogin() {
+      if (this.email === '' || this.email == null) {
+        alert('Требуется ввести Email');
+      }
+      else if (this.password === '' || this.password == null) {
+        alert('Требуется ввести пароль');
+      } else {
+        login(this.email, this.password).then((res) => {
+          if (res.fullname) {
+            alert(res.fullname)
+          } else {
+            alert(res.message);
+          }
+        })
+      }
+    }
+  }
 };
 </script>
 
--- 4-module/4-task/views/RegisterPage.vue
+++ 4-module/4-task/views/RegisterPage.vue
@@ -1,12 +1,86 @@
 <template>
-  <form class="form"></form>
+  <form class="form" @submit.prevent="doRegister">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input type="email" @input="email = $event.target.value" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Имя</label>
+      <div class="input-group">
+        <input type="text" @input="fullName = $event.target.value" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input type="password" @input="password = $event.target.value" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Повтор пароля</label>
+      <div class="input-group">
+        <input type="password" @input="checkedPassword = $event.target.value" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="checkbox"><input v-model="condition" type="checkbox" /> Я согласен с условиями <span></span></label>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary">Зарегистрироваться</button>
+    </div>
+    <div class="form__append">Уже есть аккаунт? <router-link to="/login" class="link">Войдите</router-link></div>
+  </form>
 </template>
 
 <script>
-// import { register } from '../data';
+ import { register } from '../data';
 
 export default {
   name: 'RegisterPage',
+
+  data() {
+    return {
+      fullName: null,
+      email: null,
+      password: null,
+      checkedPassword: null,
+      condition: null
+    }
+  },
+
+  methods: {
+    setEmail(event) {
+      this.email = event.target.value
+    },
+
+    doRegister() {
+      if (this.email === '' || this.email == null) {
+        alert('Требуется ввести Email');
+      }
+      else if (this.fullName === '' || this.fullName == null) {
+        alert('Требуется ввести полное имя');
+      }
+      else if (this.password === '' || this.password == null) {
+        alert('Требуется ввести пароль');
+      }
+      else if (this.checkedPassword !== this.password )  {
+        alert('Пароли не совпадают');
+      }
+      else if (!this.condition)  {
+        alert('Требуется согласиться с условиями');
+      } else {
+        register(this.email, this.fullName, this.password).then(res => {
+          if (res.fullname) {
+            alert(res.id)
+          } else {
+            alert(res.message);
+          }
+        })
+      }
+    }
+  }
 };
 </script>
 
