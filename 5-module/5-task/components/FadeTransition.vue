44a1bc2910dfe4e5a25abbdaa201b00270714e4d
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2021-02-18 18:49:32 +0100

    5-5: Transitions-1 was done

--- 5-module/5-task/components/FadeTransition.vue
+++ 5-module/5-task/components/FadeTransition.vue
@@ -1,9 +1,36 @@
-<template></template>
+<template>
+  <transition name="fade" mode="out-in">
+    <slot v-bind="$attrs"
+          v-on="listeners"></slot>
+  </transition>
+</template>
 
 <script>
 export default {
   name: 'FadeTransition',
+  inheritAttrs: false,
+
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners()
+      }
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style>
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.2s ease-in-out;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+
+</style>
--- 5-module/5-task/components/FadeTransitionGroup.vue
+++ 5-module/5-task/components/FadeTransitionGroup.vue
@@ -1,9 +1,50 @@
-<template></template>
+<template>
+  <transition-group name="fade-list">
+    <slot
+      v-bind="$attrs"
+      v-on="listeners"
+    ></slot>
+    </transition-group>
+</template>
 
 <script>
 export default {
   name: 'FadeTransitionGroup',
+  inheritAttrs: false,
+
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners()
+      }
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style>
+
+  .fade-list {
+    position: relative;
+  }
+
+  .fade-list > * {
+    transition: all 0.3s ease-out;
+  }
+
+  .fade-list-leave-active {
+    position: absolute !important;
+    left: 0;
+    right: 0;
+  }
+
+  .fade-list-enter,
+  .fade-list-leave-to {
+    opacity: 0;
+  }
+
+  .fade-list-move {
+    transition: transform 0.3s;
+  }
+
+</style>
