61a62e244550c621167336b3f4a802cd97ccd152
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2021-02-26 18:26:36 +0100

    5-5: CounterButton_last_final was done

--- 6-module/1-task/components/CounterButton.vue
+++ 6-module/1-task/components/CounterButton.vue
@@ -1,7 +1,46 @@
-<template><div></div></template>
+<template>
+  <div>
+    <button @click="increment" type="button">{{ count_ }}</button>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+
+  props: {
+    count: {
+      default: 0,
+      type: Number
+    }
+  },
+
+  data() {
+    return {
+      count_: this.count
+    }
+  },
+
+  watch: {
+    count: {
+      handler(newValue) {
+        this.count_ = newValue
+      },
+      immediate: true
+    }
+  },
+
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
+
+  methods: {
+    increment() {
+      this.$emit('increment', ++this.count_);
+    }
+  }
+
+};
 </script>
 
 <style></style>
