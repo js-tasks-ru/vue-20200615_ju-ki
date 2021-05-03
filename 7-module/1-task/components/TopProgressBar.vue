f1e80ce0c4a8009896e0712d3f6d4516033e7c88
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2021-05-03 16:59:09 +0200

    7-1: TopProgressBar

--- 7-module/1-task/components/TopProgressBar.vue
+++ 7-module/1-task/components/TopProgressBar.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="progress"></div>
+  <div class="progress"
+    :class="{
+      show,
+      failed,
+    }"
+    :style="{
+      width: currentProgress + '%',
+    }"></div>
 </template>
 
 <script>
@@ -8,9 +15,54 @@ const K = 0.975;
 const STEP_DURATION = 200;
 const HIDE_DURATION = 200;
 
+let interval = null;
+
 export default {
   name: 'TopProgressBar',
+
+  data() {
+    return {
+      currentProgress: 0,
+      show: false,
+      failed: false,
+    };
+  },
+
+  methods: {
+    start() {
+      this.currentProgress = 0;
+      this.show = true;
+      this.failed = false;
+      if (interval) {
+        clearInterval(interval);
+      }
+      this.currentProgress = 0;
+      interval = setInterval(() => {
+        this.currentProgress =
+          MAX_PROGRESS - (MAX_PROGRESS - this.currentProgress) * K;
+      }, STEP_DURATION);
+    },
+
+    async finish() {
+      clearInterval(interval);
+      interval = null;
+      this.currentProgress = 100;
+      setTimeout(() => {
+        this.show = 0;
+        setTimeout(() => {
+          this.currentProgress = 0;
+        }, HIDE_DURATION + 100);
+      }, STEP_DURATION + 200);
+    },
+
+    fail() {
+      this.failed = true;
+      this.finish();
+    },
+  },
 };
+
+
 </script>
 
 <style scoped>
@@ -20,7 +72,7 @@ export default {
   left: 0;
   right: 0;
   height: 4px;
-  width: 0;
+  width: 0px;
   opacity: 0;
   transition: opacity 0.2s linear;
   background-color: #4c6bb6;
